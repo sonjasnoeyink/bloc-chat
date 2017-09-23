4208b9686ec6d961435a43820da208b9ab8c503a
sonjasnoeyink <sonja.snoeyink@gmail.com>
2017-09-23 16:15:01 -0400

    Checkpoint 2: List Chatrooms

---
+++ app/scripts/services/Room.js
@@ -0,0 +1,15 @@
+(function() {
+  function Room($firebaseArray) {
+    var Room = {};
+    var ref = firebase.database().ref().child("rooms");
+    var rooms = $firebaseArray(ref);
+
+    Room.all = rooms;
+
+    return Room;
+  }
+
+  angular
+    .module('blocChat')
+    .factory('Room', ['$firebaseArray', Room]);
+})();
